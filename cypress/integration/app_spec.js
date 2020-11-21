c94c9e2f2730c5da0ae251e010006ab0e1515012
Jozef Sumaj <jozef.sumaj@gmail.com>
2020-11-21 21:59:35 +0100

    Test suite structure draft

--- cypress/integration/app_spec.js
+++ cypress/integration/app_spec.js
@@ -6,14 +6,68 @@
 
 // check this file using TypeScript if available
 // @ts-check
-
-describe('Assignment', function () {
+describe('Layout', () => {
   before(() => {
     cy.visit('/');
   });
+  it('Shows app header', () => {
 
-  it('tests something', () => {
+  })
+  it('Focuses to todo input', () => {
 
+  })
+  it('Shows help text', () => {
+
+  })
+})
+
+describe('Working with todos', () => {
+  before(() => {
+    cy.visit('/');
   });
+  
+  describe('Create todos', () => {
+    it('Allows to add plain todo', () => {
 
+    })
+    it('Allows to add unicode todo', () => {
+  
+    })
+    it('Doesn\'t allow to add empty todo', () => {
+  
+    })
+  })
+
+  describe('Update todos', () => {
+    before(() => {
+      cy.visit('/');
+    });
+    it('Allows to edit active todo', () => {
+  
+    })
+    it('Allows to edit completed todo', () => {
+  
+    })
+    it('Allows to cancel edit action', () => {
+  
+    })
+    it('Allows to remove todo by editing', () => {
+  
+    })
+  })
+
+  describe('Remove todos', () => {
+    before(() => {
+      cy.visit('/');
+    });
+    it('Allows to remove active todo', () => {
+  
+    })
+    it('Allows to remove completed todo', () => {
+  
+    })
+    it('Allows to bulk remove completed todos ', () => {
+  
+    })
+  })
 })
